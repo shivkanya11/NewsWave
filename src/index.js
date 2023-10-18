eac8799d225d294c632e21887d954aba651573d5
shivkanya11 <mhetreshivkanya@gmail.com>
2023-10-18 22:01:19 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './views/Home';
+import Home from './views/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
